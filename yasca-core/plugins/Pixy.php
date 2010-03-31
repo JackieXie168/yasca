ac4a63cdaf84e8a6d7b7556b662bf556fda43c61
mmaass <mmaass@48d2438e-5642-428c-99dc-fd97a2ae0f62>
2010-03-31 19:46:44 +0000

    Pixy.php now:
    -no longer reports errors to Yasca's stderror outside of Yasca's control, they are now captured in the output string in the pixy plugin.
    -now only checks the java version once per Yasca execution (stores it) rather than for every file it is invoked on.
    
    Struts.NoResetFunction.php now:
    
    -is marked as is_multi_target to reflect it's actual implementation.
    -no longer reads from an undeclared/uninitialized variable.

--- yasca-core/plugins/Pixy.php
+++ yasca-core/plugins/Pixy.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * The Pixy Plugin uses Pixy to discover potential vulnerabilities in PHP 4 files.
- * This class is a Singleton that runs only once, returning all of the results that
- * first time.
  * Uses Pixy 3.0.3
  * @extends Plugin
  * @package Yasca
@@ -12,13 +10,12 @@ class Plugin_Pixy extends Plugin {
 
     public $executable = array('Windows' => "%SA_HOME%resources\\utility\\pixy\\pixy.bat",
                                'Linux'   => "%SA_HOME%resources/utility/pixy/pixy");
-    /**
-     * This class is multi-target.
-     */
+    
+    //@todo Pixy is the #2 slowest plugin because it causes a full instantiation of the java JVM for each and every file, rather than once.
     public $is_multi_target = false;
     
     /** Shortcuts the Java check because Pixy is not multi-target */
-    private $java_not_found = false;
+    protected static $minimum_java_exists = null;
 
     public $installation_marker = "pixy";
     
@@ -27,47 +24,47 @@ class Plugin_Pixy extends Plugin {
      * process output comes back here.
      */
     function execute() {
-        if ($this->java_not_found)      // added because Pixy is a one-at-a-time plugin
-            return;
+    	// added because Pixy is a one-at-a-time plugin
+        if (isset(self::$minimum_java_exists) && !self::$minimum_java_exists) return;
         
         $yasca =& Yasca::getInstance();
         
-        if (!$this->check_for_java(1.5)) {
-            $yasca->log_message("The Pixy Plugin requires JRE 1.5 or later.", E_USER_WARNING);
-            $this->java_not_found = true;
-            return;
-	}
-        
-        $dir = $yasca->options['dir'];
+        if (!isset(self::$minimum_java_exists)){
+        	self::$minimum_java_exists = $this->check_for_java(1.5);
+	        if (!self::$minimum_java_exists) {
+	            $yasca->log_message("The Pixy Plugin requires JRE 1.5 or later.", E_USER_WARNING);
+	            return;
+	        }
+        }
+	
         $pixy_results = array();
         
         $executable = $this->executable[getSystemOS()];
         $executable = $this->replaceExecutableStrings($executable);
 
-        exec( $executable . " " . escapeshellarg($this->filename), $pixy_results);
-           
+        exec( $executable . " " . escapeshellarg($this->filename) . " 2>&1", $pixy_results);
+               
         if ($yasca->options['debug']) 
             $yasca->log_message("Pixy returned: " . implode("\r\n", $pixy_results), E_ALL);
     
         $rule = "";
         $category_link = "#";
         
-        for ($i=0; $i<count($pixy_results); $i++) {
-            $line = $pixy_results[$i];
-            if (preg_match('/^XSS Analysis BEGIN/i', $pixy_results[$i])) { 
+        foreach($pixy_results as $line) {
+            if (preg_match('/^XSS Analysis BEGIN/i', $line)) { 
             	$rule = "Cross-Site Scripting"; 
             	$category_link = "http://www.owasp.org/index.php/Cross_Site_Scripting"; 
-            } elseif (preg_match('/^SQL Analysis BEGIN/i', $pixy_results[$i])) {
+            } elseif (preg_match('/^SQL Analysis BEGIN/i', $line)) {
             	$rule = "SQL Injection";
             	$category_link = "http://www.owasp.org/index.php/SQL_Injection"; 
-            } elseif (preg_match('/^File Analysis BEGIN/i', $pixy_results[$i])) {
+            } elseif (preg_match('/^File Analysis BEGIN/i', $line)) {
             	$rule = "File-Related Vulnerability";
             	$category_link = "#";
             }
 
             if ($rule == "") continue;
             
-            if (preg_match('/^\-\d*(.*?):(\d+)$/', $pixy_results[$i], $results)) {
+            if (preg_match('/^\-\d*(.*?):(\d+)$/', $line, $results)) {
                 $vFilename = str_replace("\\", "/", trim($results[1]));
                 //if (!file_exists($vFilename)) continue;
                 $vFilename = trim($results[1]);
@@ -88,13 +85,13 @@ class Plugin_Pixy extends Plugin {
                 $result->source = $result->source[0];
                 $result->description = $yasca->get_adjusted_description("Pixy", $rule, "<p>description</p><h4>Example:</h4><pre class=\"fixedwidth\">example</pre>");
 
-		$result->source_context = array_slice( file($vFilename), max( $result->line_number-(($this->context_size+1)/2), 0), $this->context_size );
-
-		if (!in_array($result, $this->result_list)) {
-		    array_push($this->result_list, $result);
+				$result->source_context = array_slice( file($vFilename), max( $result->line_number-(($this->context_size+1)/2), 0), $this->context_size );
+	
+				if (!in_array($result, $this->result_list)) {
+				    array_push($this->result_list, $result);
+				}
+		    }
 		}
-	    }
-	}
     }
 }
 ?>
