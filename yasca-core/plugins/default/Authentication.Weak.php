b716976302e3cd896dc722616ab34470c67698bb
mmaass <mmaass@48d2438e-5642-428c-99dc-fd97a2ae0f62>
2010-04-07 20:02:16 +0000

    The category for Crypto.Weak.grep was made to be more general
    SessionInvalidation.php no longer tries to load files into memory that it can't handle
    
    Authentication.Weak.php no longer tries scan files it can't handle like mov, wmv, mp3, and gz
    
    Grep.php now:
    
    -Utilizes pre_grep in a more scalable manner (improves performance for large projects)
    -Contains documentation fixes
    -Uses about 2 MB less memory

--- yasca-core/plugins/default/Authentication.Weak.php
+++ yasca-core/plugins/default/Authentication.Weak.php
@@ -6,7 +6,7 @@
  *  foo-password = bar
  * or
  *  foo-login = quux
- *  foo-pass = quux 
+ *  foo-pass = quux
  * (etc.)
  *
  * @version 1.1 (2008-12-26)
@@ -14,35 +14,46 @@
  * @package Yasca
  */
 class Plugin_authentication_weak extends Plugin {
-    public $valid_file_types = array();
-    private $lookahead_length = 20;
+	public $valid_file_types = array();
+	protected $lookahead_length = 20;
 
-    function execute() {
-        // Handle this separately, since it's valid on all files EXCEPT those listed below
-        if (Plugin::check_in_filetype($this->filename, array("java", "jsp", "class", "jar", "zip", "jpg", "png", "gif", "exe"))) {
-            return;
-        }
-        for ($i=0; $i<count($this->file_contents); $i++) {
-            $matches = array();
-            if ( preg_match('/^(.{0,20})(user|username|logon|logonid|userid|login|loginid)\s*=\s*([^\s]+)/i', $this->file_contents[$i], $matches) ) {
-                $prefix = $matches[1];
-                $username = $matches[3];
+	public function Plugin_authentication_weak($filename, &$file_contents){
+		parent::Plugin($filename, $file_contents);
+		// Handle this separately, since it's valid on all files EXCEPT those listed below
+		// TODO White-list checking instead of blacklist checking. An 80 meg media file in the directory will crash yasca.
+		if ($this->check_in_filetype($filename, array("jar", "zip", "dll", "war", "tar", "ear",
+													  "jpg", "png", "gif", "exe", "bin", "lib",
+													  "svn-base", "7z", "rar", "gz",
+													  "mov", "wmv", "mp3"))) {
+			$this->is_valid_filetype = false;
+		}
+	}
 
-                if (strlen(trim($username, "\"\';")) == 0) {		// Fixed Bug #2143037 
-                    continue;
-                }
+	function execute() {
+		//This plugin is slow based on measurements from the profiler. Caching the counting significantly helped.
+		$count = count($this->file_contents);
+		for ($i=0; $i<$count; $i++) {
+			$matches = array();
+			if (! preg_match('/^(.{0,20})(user|username|logon|logonid|userid|login|loginid)\s*=\s*([^\s]+)/i',
+						     $this->file_contents[$i], $matches) ) continue;
+			$prefix = $matches[1];
+			$username = $matches[3];
 
-                for ($j=$i+1; $j<$i+$this->lookahead_length; $j++) {
-                    if (!isset($this->file_contents[$j])) break;
-                    $quote = preg_quote($prefix) . "pass(word)?\s*=\s*" . preg_quote($username) . "(1|123)?";
-                    $quote = str_replace("/", "\/", $quote);
-                    if ( preg_match('/' . $quote . '/i', $this->file_contents[$j]) ) {
-                        $result = new Result();
-                        $result->line_number = $i+1;
-                        $result->severity = 1;
-                        $result->category = "Authentication: Weak Credentials";
-                        $result->category_link = "http://www.owasp.org/index.php/Weak_credentials";
-                            $result->description = <<<END
+			if (strlen(trim($username, "\"\';")) == 0) {		// Fixed Bug #2143037
+				continue;
+			}
+
+			$inner_count = min($i+$this->lookahead_length, $count);
+			for ($j=$i+1; $j<$inner_count; $j++) {
+				$quote = preg_quote($prefix) . "pass(word)?\s*=\s*" . preg_quote($username) . "(1|123)?";
+				$quote = str_replace("/", "\/", $quote);
+				if ( preg_match('/' . $quote . '/i', $this->file_contents[$j]) ) {
+					$result = new Result();
+					$result->line_number = $i+1;
+					$result->severity = 1;
+					$result->category = "Authentication: Weak Credentials";
+					$result->category_link = "http://www.owasp.org/index.php/Weak_credentials";
+					$result->description = <<<END
                         <p>
                             Passwords that match the associated username are extremely weak and should never be
                             used in a production environment, even if the password happens to meet the other
@@ -55,17 +66,14 @@ class Plugin_authentication_weak extends Plugin {
                             </ul>
                         </p>
 END;
-                        array_push($this->result_list, $result);
-                        $result = null;
-                        break;
-                    }
-                }
-                $username = null;
-            }
-            $matches = null;
-        }
+					array_push($this->result_list, $result);
+					break;
+				}
+			}
+		}
 
-    }
+	}
 
 }
 ?>
+
