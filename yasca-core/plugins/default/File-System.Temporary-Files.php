8246da34f057613a46e6ad62e79d964eeb2495c6
mmaass <mmaass@48d2438e-5642-428c-99dc-fd97a2ae0f62>
2010-04-14 21:21:18 +0000

    Tweaked to reduce noise.

--- yasca-core/plugins/default/File-System.Temporary-Files.php
+++ yasca-core/plugins/default/File-System.Temporary-Files.php
@@ -1,5 +1,8 @@
 <?php
-
+require_once("lib/Common.php");
+require_once("lib/Plugin.php");
+require_once("lib/Result.php");
+require_once("lib/Yasca.php");
 /**
  * This class looks for temporary files.
  * @extends Plugin
@@ -8,48 +11,104 @@
 class Plugin_file_system_temporary_files extends Plugin {
     public $valid_file_types = array(); // match everything
     
-    public $pattern_list = array(
-                "*.tmp",
-                            "*.temp",
-                            "*dummy*",
-                            "*.old",
-                            "*.bak",
-                            "*.save",
-                            "*.backup",
-                            "*.orig",
-                            "*.000",
-                            "*.copy",
-                                "temp.*",
-                            "Copy of*",     /* Windows copy/paste */
-                            "_*",
-                            "vssver.scc",   /* Visual SourceSafe */
-                            "thumbs.db",    /* Explorer Thumbnails */
-                            "*.psd",    /* Photoshop */ 
-                "hco.log",  /* CA Harvest */
-                "harvest.sig",  /* CA Harvest */
-                "*.svn-base",   /* SVN */
-                "all-wcprops",  /* SVN */
-                ".project", /* Eclipse */
-                ".classpath",   /* Eclipse */
-                ".gitignore"    /* Git */
-                            );
+    protected static $already_executed = false;
 
-    function execute() {        
-        $filename = basename($this->filename);
-        foreach ($this->pattern_list as $pattern) {
-            if (fnmatch($pattern, $filename)) {
-                $result = new Result();
-                $result->severity = 3;
-                $result->category = "Potentially Sensitive Data Under Web Root";
-                $result->category_link = "http://www.owasp.org/index.php/Sensitive_Data_Under_Web_Root";
-                $result->description = <<<END
+    public $is_multi_target = true;
+
+    public function __construct($filename, $file_contents) {
+        if (static::$already_executed) {
+        	$this->initialized = true;
+        	return;
+        }
+        
+        parent::__construct($filename, $file_contents);
+    }
+    
+    public function execute() {        
+        if (static::$already_executed) return;
+        static::$already_executed = true;
+    	
+    	$match_once_patterns = array(
+        	"vssver.scc"	=> "Visual SourceSafe",
+    	    "*.pdb"			=> "Visual Studio Debug ",
+            "thumbs.db"		=> "Windows Explorer Thumbnail",
+            "*.psd"			=> "Raw Photoshop", 
+            "hco.log"		=> "CA Harvest Log", 
+            "harvest.sig"	=> "CA Harvest signature",
+            "*.svn-base"	=> "SVN",
+            "all-wcprops"	=> "SVN",
+    		"*.cvsignore"	=> "CVS",
+            ".project"		=> "Eclipse",
+    		".classpath"	=> "Eclipse",
+    		".buildpath"	=> "Eclipse",
+    		"org.eclipse.*prefs" => "Eclipse",
+            ".gitignore"    => "Git"
+        );
+                            
+        $match_multiple_patterns = array(
+            "*.suo"			=> "Visual Studio",
+    		"*.sln"			=> "Visual Studio",
+    		"*.csproj"		=> "Visual Studio",
+    		"*.csproj.user" => "Visual Studio",
+            "*.tmp"			=> "Temporary",
+            "*.temp"		=> "Temporary",
+            "*dummy*"		=> "Temporary",
+            "*.old"			=> "Backup",
+            "*.bak"			=> "Backup",
+            "*.save"		=> "Backup",
+            "*.backup"		=> "Backup",
+            "*.orig"		=> "Backup",
+            "*.000"			=> "Temporary",
+        	"temp.*" 		=> "Temporary",
+            "*.copy"		=> "Copy/paste",
+            "Copy of*"		=> "Windows XP and earlier copy/paste",
+    		"* - Copy"	 	=> "Windows Vista and newer copy/paste",
+            "_*"			=> "Temporary"	//@todo What systems or methods use this to denote temp files?
+        );
+        	
+        $exception_list = array(
+        	"__*__.py" /* Do not match special python files */
+        );
+		
+    	$yasca =& Yasca::getInstance();
+    	$target_basename_list = array_unique(
+    		array_map(function ($target){return basename($target);}, $yasca->target_list)
+    	); 
+    	
+    	$found_items = array();
+    	foreach ($target_basename_list as $target){
+    		foreach($exception_list as $pattern){
+    			if (fnmatch($pattern, $target)) continue 2;
+    		}
+    		foreach($match_once_patterns as $pattern => $description){
+    			if (fnmatch($pattern, $target)){
+    				$found_items[$pattern] = $description;
+    				continue 2;
+    			}
+    		}
+		foreach($match_multiple_patterns as $pattern => $description){
+		    if (fnmatch($pattern, $target)){
+    				$found_items[$target] = $description;
+    				continue 2;
+    			}
+    		}
+    	}
+    	    		
+    	foreach($found_items as $item => $description){
+	        $result = new Result();
+            $result->filename = $item;
+            $result->severity = 3;
+            $result->plugin_name = "Potentially Sensitive Data Visible";
+            $result->category = "Potentially Sensitive Data Visible";
+            $result->category_link = "http://www.owasp.org/index.php/Sensitive_Data_Under_Web_Root";
+            $result->description = <<<END
 <p>
         Temporary, backup, or hidden files should not be included in a production site because they can sometimes contain
     sensitive data such as:
         <ul>
-                <li>Source Code (e.g. index.php.old)</li>
-        <li>A list of other files (e.g. harvest.sig, .svn/*)</li>
-        <li>Deployment information (e.g. .project)</li>
+	        <li>Source Code (e.g. index.php.old)</li>
+	        <li>A list of other files (e.g. harvest.sig, .svn/*)</li>
+	        <li>Deployment information (e.g. .project)</li>
         </ul>
         These files should be removed from the source tree, or at least prior to a production rollout.
 </p>
@@ -60,13 +119,10 @@ class Plugin_file_system_temporary_files extends Plugin {
         </ul>
 </p>
 END;
-                $result->source = "This type of file is usually not used in production.";
-                $result->is_source_code = false;
-                array_push($this->result_list, $result);
-            }
+            $result->source = "$description files should not be visible on a production site.";
+            $result->is_source_code = false;
+            $this->result_list[] = $result;
         }
-        
-        
     }
 }
 ?>
